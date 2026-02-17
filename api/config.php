65c7192fcd07a940572d1f2dc41f8a06656d9bd3
X-Analyser <xanalyser.fun@gmail.com>
2026-02-17 02:29:55 -0500

    feat: add initial database configuration file for database credentials.

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+// Credenciais de Banco de Dados Local - NÃO COMMITAR
+$host = 'localhost'; 
+$db   = 'u786088869_imagens';
+$user = 'u786088869_imagens';
+$pass = 'ATLAS.vps2026';
+$charset = 'utf8mb4';
+?>
